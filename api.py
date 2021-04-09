01d889da8e1a467b8be93423487c298ff30b6be3
vikasxpandey <vikas.pandey792@gmail.com>
2021-04-09 15:57:57 +0530

    initial

---
+++ api.py
@@ -0,0 +1,18 @@
+import flask
+from flask import request, jsonify
+from vectors import words, closest_word
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['POST'])
+def home():
+    
+    return jsonify(
+            {
+                "result": closest_word(words[request.json['root']] - words[request.json['subtract']] + words[request.json['add']])
+            }
+        )
+
+app.run()
